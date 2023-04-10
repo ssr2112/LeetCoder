ff6d42d52b27b4f95de6b0b5449e0267e7365212
ssr2112 <107883000+ssr2112@users.noreply.github.com>
2023-04-11 00:09:26 +0530

    Added solution - LeetHub

---
+++ Maximum Length - GFG/maximum-length.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+public:
+    int solve(int a, int b, int c) {
+        vector<int>ch = {a,b,c};
+        sort(ch.begin(), ch.end());
+        int f=ch[0], s=ch[1], t=ch[2];
+        
+        if(2*(s+f+1)<t) return -1;
+        return a+b+c;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        Solution ob;
+        int ans = ob.solve(a, b, c);
+     
+
+        cout <<  ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
